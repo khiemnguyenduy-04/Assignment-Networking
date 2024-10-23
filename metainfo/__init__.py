f186386e23bc0fb654ba7dc333af7a54593460e8
Raven1911 <vuduclam1911@gmail.com>
2024-10-23 23:59:03 +0700

    setup metainfo

---
+++ metainfo/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+
+from .metainfo_storage import MetainfoStorage
+from .file_manager import FileManager
+
+__all__ = ['MetainfoStorage', 'FileManager']
--- metainfo/metainfo_storage.py
+++ metainfo/metainfo_storage.py
@@ -2,3 +2,40 @@
 #   store metadata in a file
 #   for tracker: info_hash
 #   for peers: support download and upload states between peers
+
+import bencodepy
+import hashlib
+
+class MetainfoStorage:
+    def __init__(self):
+        self.metainfo = {}
+        self.info_hash = None
+
+    def load_metainfo(self, torrent_file):
+        """Phân tích file .torrent và lưu trữ thông tin về tệp."""
+        with open(torrent_file, 'rb') as f:
+            self.metainfo = bencodepy.decode(f.read())
+        
+        # Tính toán info_hash
+        info = self.metainfo[b'info']
+        self.info_hash = hashlib.sha1(bencodepy.encode(info)).hexdigest()
+
+    def get_info_hash(self):
+        """Trả về info_hash của tệp từ file .torrent."""
+        return self.info_hash
+
+    def get_piece_length(self):
+        """Trả về kích thước mỗi mảnh tệp."""
+        return self.metainfo[b'info'][b'piece length']
+
+    def get_total_size(self):
+        """Trả về tổng kích thước tệp."""
+        total_size = 0
+        info = self.metainfo[b'info']
+        
+        if b'length' in info:  # Tệp đơn
+            total_size = info[b'length']
+        elif b'files' in info:  # Tệp đa
+            total_size = sum(file[b'length'] for file in info[b'files'])
+        
+        return total_size
